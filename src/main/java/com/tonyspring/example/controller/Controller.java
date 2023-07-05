f119f01528d4d099dac1669e7f3dc21c618e9ede
Tony1790 <tony45081790@gmail.com>
2023-07-06 03:26:56 +0900

    file upload

--- src/main/java/com/tonyspring/example/controller/Controller.java
+++ src/main/java/com/tonyspring/example/controller/Controller.java
@@ -1,12 +1,24 @@
 package com.tonyspring.example.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.security.Principal;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -16,13 +28,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tonyspring.example.domain.*;
 import com.tonyspring.example.service.*;
 import com.tonyspring.example.util.*;
+
+import lombok.*;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -284,6 +301,94 @@ public class Controller {
 		return "/index";
 	}
 	
+	@RequestMapping("/uploadAjax")
+	public void uploadAjax() {
+		logger.info("upload ajax");
+	}
+	
+	//오늘 날짜의 경로를 문자열로 생성.
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		Date date = new Date();
+
+		String str = sdf.format(date);
+
+		return str.replace("-", File.separator);
+
+	}
+	
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+		List<AttachFileDTO> list = new ArrayList<>();
+		
+		String uploadFolder = "C:\\upload";
+		String uploadFolderPath = getFolder();
+
+		//make Folder--------------------
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
+		
+		//해당 경로가 있는지 검사 후, 없으면 폴더를 생성함
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		//make yyyy/MM/dd 폴더
+		
+		for(MultipartFile multipartFile : uploadFile) {
+			
+			AttachFileDTO attachFileDTO = new AttachFileDTO();
+			
+			String uploadFileName = multipartFile.getOriginalFilename();
+			
+			//IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			logger.info("only file name : " + uploadFileName);
+			attachFileDTO.setFileName(uploadFileName);
+
+			UUID uuid = UUID.randomUUID();
+			
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			try {
+				File saveFile = new File(uploadPath, uploadFileName);
+				multipartFile.transferTo(saveFile);
+
+				attachFileDTO.setUuid(uuid.toString());
+				attachFileDTO.setUploadPath(uploadFolderPath);
+				
+				// Check if the file is an image type
+				if (checkImageType(saveFile)) {
+					attachFileDTO.setImage(true);
+					try (FileInputStream fis = new FileInputStream(saveFile);
+							FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName))) {
+						Thumbnailator.createThumbnail(fis, thumbnail, 100, 100);
+					}
+				}
+				
+				//리스트에 다 더하기
+				list.add(attachFileDTO);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+			} //end catch
+		}//end for
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	//업로드된 파일이 이미지 종류의 파일인지 확인
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+		} catch(IOException e) {
+			//TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
 
 
--- src/main/webapp/WEB_INF/views/board/board_before_create.jsp
+++ src/main/webapp/WEB_INF/views/board/board_before_create.jsp
@@ -8,6 +8,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <title>게시글 작성하기</title>
 <style>
 	.content {
@@ -26,5 +27,57 @@
 		<textarea class="content" name="bContent" placeholder="내용 : "></textarea>	
 		<p><input type=submit value="작성"/></p>	
 	</form>
+	
+	<div class="uploadDiv">
+		<input type="file" name="uploadFile" multiple="multiple">	
+	</div>
+	<button id="uploadBtn">업로드</button>
+	
+<script>
+	$(document).ready(function(){
+		var regex = new RegExp("(.*?)\.(exe\sh|zip|alz)$");
+		var maxSize = 10485760; //10MB
+		
+		function checkExtension(fileName, fileSize){
+			if(fileSize >= maxSize) {
+				alert("파일 사이즈를 초과했습니다.")
+				return false;
+			}
+			
+			if(regex.test(fileName)){
+				alert("해당 종류의 파일은 업로드가 불가능합니다.")
+				return false;
+			}
+			return true;
+		}
+		
+		var cloneObj = $(".uploadDiv").clone();
+		
+		$('#uploadBtn').on('click', function(e){
+			var formData = new FormData();
+			var inputFile = $("input[name='uploadFile']");
+			var files = inputFile[0].files;
+			console.log(files);
+			
+			//파일 데이터를 FormData에 추가
+			for(var i = 0; i < files.length; i++){
+				formData.append("uploadFile", files[i]);
+			}
+			
+			$.ajax({
+				url: '/uploadAjaxAction',
+					processData : false,
+					contentType : false,
+					data : formData,
+					type : "POST",
+					dataType:'JSON',
+					success: function(result) {
+						console.log(result);
+						$(".uploadDiv").html(cloneObj.html());
+					}
+			}); //ajax
+		});
+	});
+</script>	
 </body>
 </html>
